ad1b39e391d25bb50406f9a38648f40fea4596ed
rsbastos <renedossantosbastos@gmail.com>
2024-08-15 09:48:14 -0300

    :sparkles: feat - implement properties - Smartphone Class

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,7 +3,9 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo;
+        private string IMEI;
+        private int Memoria;        
 
         public Smartphone(string numero)
         {
