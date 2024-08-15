62dc69ea0157fb5b749e0f10be08bdc702200573
rsbastos <renedossantosbastos@gmail.com>
2024-08-15 10:10:52 -0300

    :sparkles: feat - implements inheritance / override method - Nokia Class

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -5,8 +5,7 @@ namespace DesafioPOO.Models
         public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
         {
 
-        }
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+        }       
         public override void InstalarAplicativo(string nomeApp)
         {
             Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone...");
